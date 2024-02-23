b78c14e50d817910e0571432d2e616533568c14a
Mares1492 <90237423+Mares1492@users.noreply.github.com>
2024-02-23 21:28:57 +0200

    Add requestAnimationFrame mode
    Add handleDraw,handleCounters,updateTimerDisplay functions for common logic
    - TIMER_MODE
    * 0 for timeout
    * 1 for animationFrame

--- GnM_prog/clock_on_the_block.js
+++ GnM_prog/clock_on_the_block.js
@@ -1,7 +1,9 @@
 let canvas;
 let ctx;
+let title;
 const MAX_COUNTER = 360
-const SPEED_MOD = 1
+const SPEED_MOD = 1 // less - faster -> 0.1 - x10
+const TIMER_MODE = 1 // 0-timeout/1-animationFrame
 
 window.onload = () =>{
     canvas =document.getElementById("canvas");
@@ -11,46 +13,94 @@ window.onload = () =>{
     const seconds = getCoordinatesBasedOnPoints(60,0.25)
     const minutes = getCoordinatesBasedOnPoints(60,0.20)
     const hours = getCoordinatesBasedOnPoints(12,0.15)
+
     let secondsIndex = 15;
     let minutesIndex = 15;
     let hoursIndex = 3;
     let secondsCounter = 0;
     let minutesCounter = 0;
     let hoursCounter = 0;
-    const initTimer = () => {
+    let startTime = null;
+
+    const handleDraw = () => {
+        drawArc();
+        drawAxis();
+        drawIndicator(seconds, secondsIndex, 1, "crimson");
+        drawIndicator(minutes, minutesIndex, 2, "orange");
+        drawIndicator(hours, hoursIndex, 3, "aqua");
+    }
+
+    const handleCounters = () =>{
+        (secondsIndex-1)<0?secondsIndex=seconds.length-1:--secondsIndex
+        secondsCounter++;
+
+        if (((60+1)^secondsCounter)===0) {
+            (minutesIndex-1)<0?minutesIndex=minutes.length-1:--minutesIndex
+            secondsCounter = 1
+            minutesCounter++
+        }
+
+        if (((60+1)^minutesCounter)===0) {
+            (hoursIndex-1)<0?hoursIndex=hours.length-1:--hoursIndex
+            minutesCounter = 1
+            hoursCounter++
+        }
+    }
+
+    const updateTimerDisplay = () =>
+        title.textContent = (
+            (hoursCounter.toString().length===2?hoursCounter:"0"+hoursCounter)
+            +":"+
+            (minutesCounter.toString().length===2?minutesCounter:"0"+minutesCounter)
+            +":"+
+            (secondsCounter.toString().length===2?secondsCounter:"0"+secondsCounter)
+        )
+
+    const initTimerUsingAnimationFrame = (timestamp) => {
+        if (!startTime) {
+            startTime = timestamp;
+        }
+        const elapsedTime = timestamp - startTime;
+    
+        if (elapsedTime >= 1000 * SPEED_MOD) {
+            handleDraw()
+            handleCounters()
+            updateTimerDisplay()
+    
+            if (secondsCounter < MAX_COUNTER) {
+                startTime = timestamp;
+                return requestAnimationFrame(initTimerUsingAnimationFrame);
+            }
+            title.textContent = "This is the end..."
+
+        }
+
+        requestAnimationFrame(initTimerUsingAnimationFrame);
+        
+    };
+    
+    const initTimerUsingTimeout = () => {
         setTimeout(()=>{
-            drawArc()
-            drawAxis()
-            drawIndicator(seconds,secondsIndex,1,"crimson");
-            drawIndicator(minutes,minutesIndex,2,"orange");
-            drawIndicator(hours,hoursIndex,3,"aqua");
-            (secondsIndex-1)<0?secondsIndex=seconds.length-1:--secondsIndex
-            secondsCounter++;
-            if (((60+1)^secondsCounter)===0) {
-                (minutesIndex-1)<0?minutesIndex=minutes.length-1:--minutesIndex
-                secondsCounter = 1
-                minutesCounter++
-            }
-            if (((60+1)^minutesCounter)===0) {
-                (hoursIndex-1)<0?hoursIndex=hours.length-1:--hoursIndex
-                minutesCounter = 1
-                hoursCounter++
-            }
-            title.textContent = (
-                (hoursCounter.toString().length===2?hoursCounter:"0"+hoursCounter)
-                +":"+
-                (minutesCounter.toString().length===2?minutesCounter:"0"+minutesCounter)
-                +":"+
-                (secondsCounter.toString().length===2?secondsCounter:"0"+secondsCounter)
-            )
+            handleDraw()
+            handleCounters()
+            updateTimerDisplay()
             
             if (secondsCounter<MAX_COUNTER) {
-                return initTimer()
+                return initTimerUsingTimeout()
             }
+
             title.textContent = "This is the end..."
         },1000*SPEED_MOD)
     }
-    initTimer()
+    
+    switch (TIMER_MODE) {
+        case 0:
+            initTimerUsingTimeout()
+            break;
+        case 1:
+            requestAnimationFrame(initTimerUsingAnimationFrame);
+            break;
+    }
 }
 
 const drawArc = () => {
