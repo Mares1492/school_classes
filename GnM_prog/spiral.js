70cf4401265b3c1d05d4d974a3218db3344e96d4
Mares1492 <90237423+Mares1492@users.noreply.github.com>
2024-05-23 01:49:49 +0300

    Update spiral.js
    - adjust colors change
    - adjust line width
    - adjust angle change

--- GnM_prog/spiral.js
+++ GnM_prog/spiral.js
@@ -1,41 +1,80 @@
-const spiralTightness = 10;
-
+let spiralTightness = 5;
 let canvas;
 let ctx;
 let title;
 let centerX;
 let centerY;
 let angle = 0;
+let red = 255, green = 255, blue = 255;
 let  isGrowing = true;
 let points = [];
+let isColorGrowing = false;
 
 const getRandomInt = (min,max) => Math.floor(Math.random()*(max-min+1))+min;
 
 const drawStep = () => {
     if (isGrowing) {
-        angle+=0.1;
-        if (angle>80) {
+        angle+=0.03;
+        if (angle>60) {
             isGrowing = false;
             angle = 0;
         }
     }
 }
 
+const modifyColor = () => {
+    console.log(red,green,blue)
+    if (isColorGrowing) {
+        if (red < 0) {
+            red-=2;
+            return;
+        }
+        if(blue > 0) {
+            blue-=2;
+            return;
+        }
+        if (green > 0) {
+            green-=2;
+            return;
+        }
+        isColorGrowing = false;
+        return;
+    }
+    if (red < 255) {
+        red+=2;
+        return;
+    }
+    if(blue < 255) {
+        blue+=2;
+        return;
+    }
+    if (green < 255) {
+        green+=2;
+        return;
+    }
+    isColorGrowing = true;
+}
+
 const drawSpiral = () => {
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.strokeStyle = "white";
     drawStep();
     const newRadius = spiralTightness * angle;
+    spiralTightness += 0.005;
 
     if (isGrowing) {
         const x = centerX + newRadius * Math.cos(angle);
         const y = centerY + newRadius * Math.sin(angle);
-        points.push({x,y,c:`rgb(${getRandomInt(0,255)} ${getRandomInt(0,255)} ${getRandomInt(0,255)})`});
+        const localRed = red;
+        const localGreen = green;
+        const localBlue = blue;
+        modifyColor();
+        points.push({x,y,r:newRadius*0.1,c:`rgb(${localRed} ${localGreen} ${localBlue})`});
     }
     else{
         points.pop();
         if(!points.length){
             isGrowing = true;
+            spiralTightness = 1;
         }
     }
     
@@ -46,8 +85,9 @@ const drawSpiral = () => {
             const prevPoint = points[i-1];
             const point = points[i];
             ctx.moveTo(prevPoint.x,prevPoint.y);
-            ctx.lineWidth = i*0.02;
+            ctx.lineWidth = point.r;
             ctx.strokeStyle = point.c;
+            prevPoint.c = point.c
             ctx.lineTo(point.x,point.y);ctx.stroke();    
             ctx.closePath();   
         }
