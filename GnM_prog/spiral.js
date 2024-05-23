c50cb6efd328f8a4af0283d9455f3a8fa9388999
Mares1492 <90237423+Mares1492@users.noreply.github.com>
2024-05-23 03:00:13 +0300

    Add sound effects

--- GnM_prog/spiral.js
+++ GnM_prog/spiral.js
@@ -1,3 +1,8 @@
+let audioCtx;
+let oscillator;
+let gainNode;  
+const OSCILLATOR_TYPES = ["sine","square","triangle","sawtooth"]
+
 let spiralTightness = 5;
 let canvas;
 let ctx;
@@ -6,9 +11,11 @@ let centerX;
 let centerY;
 let angle = 0;
 let red = 255, green = 255, blue = 255;
-let  isGrowing = true;
+let isGrowing = true;
 let points = [];
 let isColorGrowing = false;
+let oscillator_type = 0;
+let volume = 50;
 
 const getRandomInt = (min,max) => Math.floor(Math.random()*(max-min+1))+min;
 
@@ -23,7 +30,6 @@ const drawStep = () => {
 }
 
 const modifyColor = () => {
-    console.log(red,green,blue)
     if (isColorGrowing) {
         if (red < 0) {
             red-=2;
@@ -67,17 +73,18 @@ const drawSpiral = () => {
         const localRed = red;
         const localGreen = green;
         const localBlue = blue;
+        const a = angle;
         modifyColor();
-        points.push({x,y,r:newRadius*0.1,c:`rgb(${localRed} ${localGreen} ${localBlue})`});
+        points.push({x,y,r:newRadius*0.1,a,c:`rgb(${localRed} ${localGreen} ${localBlue})`});
     }
     else{
         points.pop();
         if(!points.length){
             isGrowing = true;
             spiralTightness = 1;
+
         }
     }
-    
 
     for (let i = 0; i < points.length; i++) {
         if (points.length>1 && i>0) {
@@ -90,6 +97,10 @@ const drawSpiral = () => {
             prevPoint.c = point.c
             ctx.lineTo(point.x,point.y);ctx.stroke();    
             ctx.closePath();   
+            if (oscillator !== undefined) {
+                oscillator.frequency.setValueAtTime(point.a*30, audioCtx.currentTime);
+                gainNode.gain.setValueAtTime(volume+point.a, audioCtx.currentTime)
+            }
         }
     }
 
@@ -102,8 +113,28 @@ window.onload = () =>{
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     title = document.getElementById("title");
+    const body = document.getElementById("body")
+    const btn = document.createElement("button")
     title.textContent = "This is the spiral process";
     centerX = canvas.width*0.5;
     centerY = canvas.height*0.5;
+    
+
+    btn.className = "sound-btn";
+    btn.textContent = "Play Sound"
+    body.appendChild(btn);
+    btn.addEventListener("click",()=>{
+        if (audioCtx === undefined) {
+            audioCtx = new AudioContext();
+            oscillator = audioCtx.createOscillator();
+            gainNode = audioCtx.createGain()
+            oscillator.type = OSCILLATOR_TYPES[getRandomInt(0,OSCILLATOR_TYPES.length-1)];
+            oscillator.connect(gainNode);
+            gainNode.connect(audioCtx.destination);
+            oscillator.start();
+            body.removeChild(btn);
+
+        }
+    });
     drawSpiral();
 }
